3d0c5f9f3fdfde8ec1a5fa3ed82b58ac9766997a
speenoo <123399952+speenoo@users.noreply.github.com>
2025-09-23 16:32:31 +0100

    Update _document.tsx

--- apps/web/pages/_document.tsx
+++ apps/web/pages/_document.tsx
@@ -23,39 +23,7 @@ export default class _Document extends Document<MyDocumentProps> {
     return (
       <Html>
         <Head>
-          <link rel="icon" href="/favicon-light.ico" />
-          {publicRuntimeConfig.NEXT_PUBLIC_GTM_ID && this.props.isAuthPage && (
-            <script
-              dangerouslySetInnerHTML={{
-                __html: `
-                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','${publicRuntimeConfig.NEXT_PUBLIC_GTM_ID}')
-              `,
-              }}
-            />
-          )}
-          {publicRuntimeConfig.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID && this.props.isAuthPage && (
-            <>
-              <script
-                async
-                src={`https://www.googletagmanager.com/gtag/js?id=${publicRuntimeConfig.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
-              />
-              <script
-                dangerouslySetInnerHTML={{
-                  __html: `
-                  window.dataLayer = window.dataLayer || [];
-                  function gtag(){dataLayer.push(arguments);}
-                  gtag('js', new Date());
-                  
-                  gtag('config', '${publicRuntimeConfig.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}');
-                  `,
-                }}
-              />
-            </>
-          )}
+          <div></div>
         </Head>
         <body>
           <Main />
